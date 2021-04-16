3645fc280c167171ffb3f0c98cc8f2f78b5c22f2
Nguyen Cong Dat <traceformula2000@gmail.com>
2021-04-17 05:24:21 +0700

    Frontend | Android | Main Activity | Create bottom navigation

--- frontend/tmdb-android/app/src/main/java/com/example/tmdbandroid/screen/main/MainActivity.java
+++ frontend/tmdb-android/app/src/main/java/com/example/tmdbandroid/screen/main/MainActivity.java
@@ -1,34 +1,50 @@
 package com.example.tmdbandroid.screen.main;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 
 import com.example.tmdbandroid.R;
 import com.example.tmdbandroid.screen.detail.DetailActivity;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
-
-    private Button toDetailBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        toDetailBtn = findViewById(R.id.toDetailBtn);
-        toDetailBtn.setOnClickListener(
-                new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        Intent i = new Intent(MainActivity.this, DetailActivity.class);
-                        i.putExtra("screenName","DETAIL");
-                        startActivity(i);
-                    }
-                }
-        );
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+        bottomNav.setOnNavigationItemSelectedListener(navListener);
+
     }
+
+    private BottomNavigationView.OnNavigationItemSelectedListener navListener =
+            new BottomNavigationView.OnNavigationItemSelectedListener() {
+                @Override
+                public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                    Fragment selectedFragment = null;
+                    switch (item.getItemId()) {
+                        case R.id.nav_home:
+                            selectedFragment = new HomeFragment();
+                            break;
+                        case R.id.nav_watchlist:
+                            selectedFragment = new WatchlistFragment();
+                            break;
+                        case R.id.nav_search:
+                            selectedFragment = new SearchFragment();
+                            break;
+                    }
+                    getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                            selectedFragment).commit();
+                    return true;
+                }
+            };
 }
---
+++ frontend/tmdb-android/app/src/main/java/com/example/tmdbandroid/screen/main/WatchlistViewModel.java
@@ -0,0 +1,7 @@
+package com.example.tmdbandroid.screen.main;
+
+import androidx.lifecycle.ViewModel;
+
+public class WatchlistViewModel extends ViewModel {
+    // TODO: Implement the ViewModel
+}
