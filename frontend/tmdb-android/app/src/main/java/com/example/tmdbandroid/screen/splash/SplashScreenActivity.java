649c0a4f1352a35d945a3249602950792ab32e48
Nguyen Cong Dat <traceformula2000@gmail.com>
2021-04-25 17:59:40 +0700

    Frontend | Android | SplashActivity | Implement splash screen

---
+++ frontend/tmdb-android/app/src/main/java/com/example/tmdbandroid/screen/splash/SplashScreenActivity.java
@@ -0,0 +1,17 @@
+package com.example.tmdbandroid.screen.splash;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.tmdbandroid.screen.main.MainActivity;
+
+public class SplashScreenActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        startActivity(new Intent(SplashScreenActivity.this, MainActivity.class));
+        finish();
+    }
+}
